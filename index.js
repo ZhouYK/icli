986365715f3e47e981823e5306faa694132837cc
yunkui.zhou <yunkui.zhou@bkjk.com>
2019-01-16 18:03:10 +0800

    initial

--- index.js
+++ index.js
@@ -1,43 +1,89 @@
 #!/usr/bin/env node
-'use strict'
-const cp = require('child_process')
-process.stdin.setEncoding('utf8')
-const infoText = ['请输入姓名：', '请输入年龄：', '请输入性别：']
-const infoArr = []
-let index = 0
-let nextText = infoText[index]
-const confirm = (info) => {
-  return `请确认您的信息：${info.join(' ')}，y/n？`
-}
-process.stdout.write(nextText)
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (index > infoText.length - 1) {
-    nextText = confirm(infoArr)
-  }
-  if (chunk !== null) {
-    const temp = chunk.substring(0, chunk.length - 1)
-    if (!temp) return process.stdout.write(nextText)
-    if (index > infoText.length - 1) {
-      nextText = confirm(infoArr)
-      if (temp === 'y') {
-        return process.stdin.push(null)
-      }
-    } else {
-      infoArr[index] = temp
-      index = index + 1
-      if (index > infoText.length - 1) {
-        nextText = confirm(infoArr)
-      } else {
-        nextText = infoText[index]
-      }
-    }
-    process.stdout.write(`${nextText}`);
-  }
-})
+const program = require('commander');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+const cp = require('child_process');
 
-process.stdin.on('end', () => {
-  'use strict'
-  console.log(`正在拉取代码仓库...`)
-  cp.execSync(`git clone --verbose --progress https://github.com/monkeyInShell/react-redux-iapp.git`)
-})
+const version = '0.0.1';
+
+const deleteFolder = (filePath) => {
+  if (fs.existsSync(filePath)) {
+    const files = fs.readdirSync(filePath);
+    files.forEach(file => {
+      const nextFolderPath = path.resolve(filePath, `./${file}`);
+      const state = fs.statSync(nextFolderPath);
+      if (state.isDirectory()) {
+        deleteFolder(nextFolderPath)
+      } else {
+        fs.unlinkSync(nextFolderPath);
+      }
+    });
+    fs.rmdirSync(filePath);
+  }
+};
+const scaffoldGitRepo = {
+  npm: 'git@github.com:ZhouYK/npm-scaffold.git',
+  'page-javascript': 'git@github.com:ZhouYK/javascript-web-framework.git',
+  'page-typescript': 'git@github.com:ZhouYK/typescript-web-framework.git',
+  node: ''
+}
+
+program
+  .version(version, '-v, --version')
+  .option('-l, --scaffold-list', 'show scaffold list')
+  .option('-s, --select-scaffold', 'show scaffold select list')
+  .parse(process.argv);
+
+if (program.scaffoldList) {
+  const text = `Scaffold：
+  1, npm
+  2, page(js)
+  3, page(ts)
+  4, node(TODO)
+  `;
+  console.log(text)
+} else if (program.selectScaffold) {
+  inquirer.prompt([{
+    type: 'list',
+    name: 'scaffold',
+    message: 'scaffold list:',
+    choices: [{
+      name: 'npm',
+      value: 'npm',
+      short: 'npm',
+    }, {
+      name: 'page(js)',
+      value: 'page-javascript',
+      short: 'page-javascript',
+    }, {
+      name: 'page(ts)',
+      value: 'page-typescript',
+      short: 'page-javascript',
+    }, {
+      name: 'node',
+      value: 'node',
+      short: 'node',
+    }]
+  }]).then(choice => {
+    // Use user feedback for... whatever!!
+    inquirer.prompt({
+      type: 'input',
+      name: 'folder',
+      message: 'folder name:'
+    }).then(answer => {
+      const p = path.resolve(process.cwd(), `./${answer.folder}`);
+      const flag = fs.existsSync(p);
+      if (!flag) {
+        fs.mkdirSync(p);
+        console.log('Digging...');
+        cp.execSync(`git clone ${scaffoldGitRepo[choice.scaffold]} ${answer.folder}`);
+        const gitPath = path.resolve(p, './.git');
+        deleteFolder(gitPath);
+        console.log('Done! Enjoy your work!');
+      } else {
+        console.warn(`folder：${answer.folder} already exists`);
+      }
+    })
+  });
+};
